3cbc9f99fdba80e6e21746fbd4b39302d24a0424
frank_oviedo <oviedofrank06@gmail.com>
2026-02-12 00:01:52 -0500

    fix: exportar app en firebase-config

--- frontend/firebase-config.js
+++ frontend/firebase-config.js
@@ -1,10 +1,5 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"; // ✅
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBtaZ2QOrYIJtOz-Gt83wAsnwSF7EyX2QI",
   authDomain: "multinegocio-4eb32.firebaseapp.com",
@@ -14,5 +9,4 @@ const firebaseConfig = {
   appId: "1:102367181742:web:af81c98e9f5fdec1b5263f"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
